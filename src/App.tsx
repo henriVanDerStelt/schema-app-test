2f6bef50f0d9596d7d5cb5ef8838a5684a35ac7d
Henri van der Stelt <hj.vdstelt@gmail.com>
2025-09-29 10:55:34 +0200

    app added some labels

--- src/App.tsx
+++ src/App.tsx
@@ -53,26 +53,34 @@ function App() {
 
   return (
     <div style={{ padding: 20 }}>
-      <h1>🏋️ My Workouts</h1>
-
+      <h3>🏋️ My Workouts</h3>
+      <p>This is a test!</p>
+      <label>Exercise name: </label>
       <input
+        id="name"
         placeholder="Exercise name"
         value={name}
         onChange={(e) => setName(e.target.value)}
       />
+      <label>Weight: </label>
       <input
+        id="weight"
         type="number"
         placeholder="Weight (kg)"
         value={weight}
         onChange={(e) => setWeight(Number(e.target.value))}
       />
+      <label>Reps: </label>
       <input
+        id="reps"
         type="number"
         placeholder="Reps"
         value={reps}
         onChange={(e) => setReps(Number(e.target.value))}
       />
+      <label>RPE: </label>
       <input
+        id="rpe"
         type="number"
         placeholder="RPE"
         value={rpe}
